f9e1e5d3efb37fe18f1ecb1602e3545392e0262e
Div12345 <47829318+Div12345@users.noreply.github.com>
2019-02-27 01:49:18 +0530

    Update Node2.ino

--- Node2.ino
+++ Node2.ino
@@ -33,8 +33,17 @@ unsigned long lastUpdate=0;
 // set up the feed
 AdafruitIO_Feed *tank_3 = io.feed("tank-3");
 // set up the feed
-AdafruitIO_Feed *turbidity = io.feed("turbidity");
+AdafruitIO_Feed *ph = io.feed("ph");
 
+//pH
+#define Offset 0.00 //deviation compensate
+#define LED 13
+#define samplingInterval 20
+#define printInterval 800
+#define ArrayLenth 40 //times of collection
+
+int pHArray[ArrayLenth]; //Store the average value of the sensor feedback
+int pHArrayIndex = 0;
 
 void setup() 
 {  
@@ -61,8 +70,8 @@ void loop() {
   //Ultrasound
   level1();
 
-  //Turbidity
-  read_turbidity();
+  //pH
+  getPH();
   
   // update timer
   lastUpdate = millis();
@@ -96,11 +105,58 @@ int distance= duration*0.034/2;
   last1 = distance;
   }
 
-void read_turbidity(){
-  double avg_ntu = 0 ,ntu;
-  int sensorValue = analogRead(A0);
-  float voltage = sensorValue * (5.0 / 1024.0);
-  ntu = ((-1120.4)*voltage*voltage)+(5742.3*voltage) - 4352.9;
-  avg_ntu += (ntu);
-  turbidity->save(avg_ntu);
+
+
+void getPH(void) 
+{
+  static float pHValue, voltage;
+    delay(samplingInterval);
+    pHArray[pHArrayIndex++] = analogRead(analog);
+    if (pHArrayIndex == ArrayLenth)pHArrayIndex = 0;
+    voltage = avergearray(pHArray, ArrayLenth) * 5.0 / 1024;
+    pHValue = 3.5 * voltage + Offset;
+    ph->save(pHValue);
+}
+
+double avergearray(int* arr, int number) 
+{
+  int i;
+  int max, min;
+  double avg;
+  long amount = 0;
+  if (number <= 0) {
+    return 0;
   }
+  if (number < 5) { //less than 5, calculated directly statistics
+    for (i = 0; i < number; i++) {
+      amount += arr[i];
+    }
+    avg = amount / number;
+    return avg;
+  }
+  else {
+    if (arr[0] < arr[1]) {
+      min = arr[0]; max = arr[1];
+    }
+    else {
+      min = arr[1]; max = arr[0];
+    }
+    for (i = 2; i < number; i++) {
+      if (arr[i] < min) {
+        amount += min; //arr<min
+        min = arr[i];
+      }
+      else {
+        if (arr[i] > max) {
+          amount += max; //arr>max
+          max = arr[i];
+        }
+        else {
+          amount += arr[i]; //min<=arr<=max
+        }
+      }
+    }
+    avg = (double)amount / (number - 2);
+  }
+  return avg;
+}
